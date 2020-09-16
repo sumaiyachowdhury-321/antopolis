b075cdd549743a3ea46838d35887c7c2661b7492
sumaiyachowdhury-321 <tanzilasiddika321@gmail.com>
2020-09-16 17:31:26 +0600

    working on cart price update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,6 +2,7 @@ $('.btn-plus, .btn-minus').on('click', function(e) {
     const isNegative = $(e.target).closest('.btn-minus').is('.btn-minus');
     const input = $(e.target).closest('.input-group').find('input');
     if (input.is('input')) {
-        input[0][isNegative ? 'stepDown' : 'stepUp']()
+        input[0][isNegative ? 'stepDown' : 'stepUp']();
+        updateQuantity();
     }
 });
